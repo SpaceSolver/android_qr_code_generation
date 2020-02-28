e14502f06191534612d438f217dc1a688aaf66b5
ssc-bando <satoshi_bando@solver.co.jp>
2020-02-28 11:04:01 +0900

    add: show sample image to learn imageview operation

--- app/src/main/java/com/example/generate_qr_code/MainActivity.java
+++ app/src/main/java/com/example/generate_qr_code/MainActivity.java
@@ -2,10 +2,15 @@ package com.example.generate_qr_code;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.res.AssetManager;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TextView;
+
+import java.io.InputStream;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -25,8 +30,16 @@ public class MainActivity extends AppCompatActivity {
 
     public void onResetButtonClicked(View view) {
         codeText.setText(R.string.default_text_code);
+        imageView.setImageBitmap(null);
     }
 
     public void onTestButtonClicked(View view) {
+        AssetManager assets = getResources().getAssets();
+        try (InputStream istream = assets.open("chuka_ryouriya.png")){
+            Bitmap bitmap = BitmapFactory.decodeStream(istream);
+            imageView.setImageBitmap(bitmap);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
